4813a6847c55ef1caea7adcca49fd2d25435ad8d
annick <annickaubert92@gmail.com>
2021-05-09 02:12:12 -0300

    resolver esto

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,14 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/2.2/ho  wto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '=4x9y8hn$aua6%5x+cvv49e%31*y%ug(%#o91&y3hvf*fd&b8('
